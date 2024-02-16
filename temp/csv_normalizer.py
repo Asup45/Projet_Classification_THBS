4567e9f5f3ec23b81989c45466420ffacc34d34a
Kodascii <kodascii.lab@gmail.com>
2024-02-16 14:00:11 +0100

    Transactions Update

--- temp/csv_normalizer.py
+++ temp/csv_normalizer.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from helpers import norm2float
+from helpers import *
 
 transactions = pd.read_csv('../res/credit_card_fraud.csv', sep=';', index_col='transactionId')
-transactions[['type']] = transactions[['type']].replace(['CASH_IN', 'CASH_OUT', 'DEBIT', 'PAYMENT', 'TRANSFER'], range(0, 5))
-transactions = norm2float(transactions, ['amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest'])
 
+transactions[['type']] = transactions[['type']].replace(['CASH_IN', 'CASH_OUT', 'DEBIT', 'PAYMENT', 'TRANSFER'], range(0, 5))
 transactions['nameOrig'] = transactions['nameOrig'].apply(replace_first_letter)
 transactions['nameDest'] = transactions['nameDest'].apply(replace_first_letter)
+transactions = norm2float(transactions, ['amount', 'oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest', 'nameOrig', 'nameDest'])
 
 transactions.to_csv('../database/transactions.csv', index='transactionId', sep=',')
