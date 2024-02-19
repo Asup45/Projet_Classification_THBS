483ea929ff0b2e5bbe5ada0ca947b8829f685b7e
Kodascii <kodascii.lab@gmail.com>
2024-02-19 14:10:39 +0100

    Flask

--- temp/datainit.py
+++ temp/datainit.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 transactions = pd.read_csv('../database/transactions.csv', index_col='transactionId')
 
-X = transactions.drop(columns=['isFraud', 'nameOrig', 'nameDest'])
+X = transactions.drop(columns=['isFraud'])
 y = transactions['isFraud']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
