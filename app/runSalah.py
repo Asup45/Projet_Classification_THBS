2f22ecd5b2cfef1b99788ca5eb3fb6bc2585c29a
Github2199 <150786148+Github2199@users.noreply.github.com>
2024-02-21 11:16:24 +0100

    maj

--- app/runSalah.py
+++ app/runSalah.py
@@ -4,13 +4,14 @@ import mysql.connector as msql
 from mysql.connector import Error
 
 app = Flask(__name__)
+app.secret_key = 'azerty'
 
 # Configuration de la connexion à MySQL
 config_bdd = {
     'host': 'localhost',
     'user': 'root',
     'password': 'admin',
-    'database': 'projet_fraude'
+    'database': 'projet_fraudedb'
 }
 
 # pour client et opérateur:Fonction pour valider les identifiants de l'utilisateur dans bdd
@@ -18,48 +19,48 @@ def valider_utilisateur(nom_utilisateur, mot_de_passe):
     try:
         conn = msql.connect(**config_bdd)
         curseur = conn.cursor()
-        requete = "SELECT email, pass, role FROM utilisateur WHERE nom_utilisateur = %s AND mot_de_passe = %s"
+        requete = "SELECT email, pass, role FROM utilisateur WHERE email = %s AND pass = %s"
         curseur.execute(requete, (nom_utilisateur, mot_de_passe))
         utilisateur = curseur.fetchone()
         curseur.close()
         conn.close()
         return utilisateur
-    except msql.connector.Error as err:
+    except Error as err:
         print(f"Erreur : {err}")
         return None
 
     
 # seulement pour client:Fonction pour creer (ou maj password) les identifiants de l'utilisateur dans bdd
-def creer_utilisateur(nom_utilisateur, mot_de_passe):
+def creer_utilisateur(nom_utilisateur, mot_de_passe,role):
     try:
         conn = msql.connect(**config_bdd)
         cursor = conn.cursor()
 
         # Vérifier si l'utilisateur existe
-        requete_utilisateur = "SELECT email, pass, role FROM utilisateur WHERE nom_utilisateur = %s"
-        cursor.execute(requete_utilisateur, (nom_utilisateur))
+        requete_utilisateur = "SELECT email, pass, role FROM utilisateur WHERE email = %s"
+        cursor.execute(requete_utilisateur, (nom_utilisateur,))
         existe_utilisateur = cursor.fetchone()
 
         if existe_utilisateur:
             # L'utilisateur existe mettre à jour le mot de passe
-            maj_requete = "UPDATE utilisateur SET mot_de_passe = %s WHERE nom_utilisateur = %s"
+            maj_requete = "UPDATE utilisateur SET pass= %s WHERE email = %s"
             cursor.execute(maj_requete, (mot_de_passe, nom_utilisateur))
         else:
             # L'utilisateur n'existe pas: le créer
-            insertion_requete = "INSERT INTO utilisateur (nom_utilisateur, mot_de_passe) VALUES (%s, %s,%s)"
-            cursor.execute(insertion_requete, (nom_utilisateur, mot_de_passe,'client'))
+            insertion_requete = "INSERT INTO utilisateur (email, pass,role) VALUES (%s, %s,%s)"
+            cursor.execute(insertion_requete, (nom_utilisateur, mot_de_passe,role))
 
         conn.commit()
         cursor.close()
         conn.close()
         return True
-    except msql.Error as err:
+    except Error as err:
         print(f"Erreur : {err}")
         return False
     
 # Insérer la ligne admin@moneyshied.fr, admin, operateur s'il n'existe pas
 if not valider_utilisateur('admin@moneyshield.fr', 'admin'):
-    creer_utilisateur('admin@societe.fr', 'admin', 'operateur')
+    creer_utilisateur('admin@moneyshield.fr', 'admin', 'operateur')
 
 @app.route('/')
 def index():
@@ -68,18 +69,22 @@ def index():
 @app.route('/', methods=['POST'])
 # formulaire operateur(connexion)/client(connexion-inscription) 
 def connexion():
-    if request.form['validation']=='connexion': # client/operateur en mode connexion 
-        nom_utilisateur = request.form['email']
-        mot_de_passe = request.form['password']
-
+    print("form :",request.form['email'])
+    print("form :",request.form['password'])
+    print("form :",request.form['validation'])
+    nom_utilisateur = request.form['email']
+    mot_de_passe = request.form['password']
+    validation_type = request.form['validation']
+    if validation_type == 'connexion':
+    #if request.form['validation']=='connexion': # client/operateur en mode connexion 
         utilisateur = valider_utilisateur(nom_utilisateur, mot_de_passe)
 
         if utilisateur:
             # Identifiants valides, rediriger en fonction du rôle
             if utilisateur[2] == 'operateur': #test du role operateur pour identification pour renvoyer sur  prediction
-                return render_template('prediction.html')
+                return render_template('prediction.html',nom_utilisateur)
             else:                             #test du role client pour identification pour renvoyer sur  transaction
-                return render_template('transaction.html')
+                return render_template('transaction.html',nom_utilisateur)
         else:
             # Identifiants invalides=> index avec message erreur
             flash("Identifiants invalides. Veuillez réessayer.")
@@ -89,11 +94,11 @@ def connexion():
         nom_utilisateur = request.form['email']
         mot_de_passe = request.form['password']
 
-        utilisateur = creer_utilisateur(nom_utilisateur, mot_de_passe)
+        utilisateur = creer_utilisateur(nom_utilisateur, mot_de_passe,'client')
 
         if utilisateur:
             # creation client et Identifiant valide 
-            return render_template('transaction.html')
+            return render_template('transaction.html',nom_utilisateur )
         else:
             # creation invalide=> index avec message erreur
             flash("Problème création client. Veuillez réessayer")
